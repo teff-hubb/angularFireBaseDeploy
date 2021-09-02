128e066dad554f2e3eb3e7ec2a2e444b998e5d78
teff-hubb <stepxan@yahoo.es>
2021-09-02 15:29:47 +0200

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB7Yy4FKdSD1B3Admdjliq3B7iUIOet8ik",
-    authDomain: "airbandb-f7164.firebaseapp.com",
-    projectId: "airbandb-f7164",
-    storageBucket: "airbandb-f7164.appspot.com",
-    messagingSenderId: "13341562085",
-    appId: "1:13341562085:web:4454e834a9a44cce7af00c"
+    apiKey: "AIzaSyDrQdLY2bRuWJpsRW0Zc8fJS2ESY7fPvKM",
+    authDomain: "airbnb-161d6.firebaseapp.com",
+    projectId: "airbnb-161d6",
+    storageBucket: "airbnb-161d6.appspot.com",
+    messagingSenderId: "569206789533",
+    appId: "1:569206789533:web:1b570191f24ec6a3dbcb74"
   }
 };
